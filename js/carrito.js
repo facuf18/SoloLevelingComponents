7b38b95f4713eb88288756d1f040b976182382db
Facundo <facufaccioli18@gmail.com>
2021-11-16 22:18:22 -0300

    correcciones tabla de componentes y agregado de precio final

--- js/carrito.js
+++ js/carrito.js
@@ -1,8 +1,11 @@
 let carritoDeComponentes = [];
+let preciosComponentes = [];
 
 function agregarAlCarrito(nuevoComponente) {
     carritoDeComponentes.push(nuevoComponente);
+    preciosComponentes.push(nuevoComponente.precio);
     console.log(carritoDeComponentes);
+    console.log(preciosComponentes);
     Swal.fire(
         'Nuevo componente agregado',
         nuevoComponente.tipo + " " + nuevoComponente.marca + " " + nuevoComponente.modelo,
@@ -11,12 +14,27 @@ function agregarAlCarrito(nuevoComponente) {
     localStorage.setItem("miPC", JSON.stringify(carritoDeComponentes));
 }
 
+let precioTotal;
+
+function calcularPrecioTotal() {
+    precioTotal = 0;
+    for (let i = 0; i < preciosComponentes.length; i++) {
+        precioTotal += preciosComponentes[i];
+    }
+    console.log(precioTotal);
+}
+
 function mostrarCarrito() {
     $("td").remove();
+    calcularPrecioTotal();
     for (const componente of carritoDeComponentes) {
         $("#tablaCarrito").append(`
             <tr><td>${componente.tipo}</td>
             <td>${componente.marca} ${componente.modelo}</td>
             <td>$${componente.precio}</td></tr>`);
     }
+    $("#tablaPrecio").append(`
+            <tr><td><b>TOTAL</b></td>
+            <td></td>
+            <td><b>$${precioTotal}</b></td></tr>`);
 }
